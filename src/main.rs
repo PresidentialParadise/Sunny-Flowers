50dda970bbff411c98d68f335945bb453d076d67
Sophie Atkins <tileyratkins@gmail.com>
2021-09-18 20:41:42 +0200

    Added Tokio signal watching

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,10 @@ use crate::discord::create_bot;
 #[tokio::main]
 async fn main() {
     create_bot().await;
+    match tokio::signal::ctrl_c().await {
+        Ok(()) => println!("Received Ctrl-C, shutting down."),
+        Err(e) => {
+            eprintln!("Unable to listen for shutdown signlar {}", e);
+        }
+    }
 }
